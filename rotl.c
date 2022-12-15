5ab464e356945d4d8f58b3f1918977cf4f032e1c
Precious Okwukwe Amaechi <poamaechi@outlook.com>
2022-12-15 11:46:36 +0100

    Rotate Function for Monty

--- rotl.c
+++ rotl.c
@@ -4,13 +4,11 @@
  * f_rotl- function that rotates the stack to the top
  * @head: head of the stack
  * @counter: line count
- * 
  * Retrun: nothing
 */
 void f_rotl(stack_t **head, __attribute__((unused)) unsigned int counter)
 {
     stack_t *tmp = *head, *aux;
-
     if (*head == NULL || (*head)->next == NULL)
     {
         return;
